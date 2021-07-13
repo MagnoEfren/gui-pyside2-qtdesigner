9f78ee33f910232ee2a825f69f56036b2cf7ba40
MagnoEfren <58579707+MagnoEfren@users.noreply.github.com>
2021-07-13 14:07:59 -0500

    Add files via upload

---
+++ Graficas Matplotlib/main.py
@@ -0,0 +1,100 @@
+import sys
+import numpy as np
+from ui_diseño import*
+from PySide2 import QtCore
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+
+class MiApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow() 
+        self.ui.setupUi(self)
+
+        self.grafica = Canvas_grafica()
+        self.grafica1 = Canvas_grafica2()
+        self.grafica2 = Canvas_grafica3()
+        self.grafica3 = Canvas_grafica4()
+
+        self.ui.grafica_uno.addWidget(self.grafica)
+        self.ui.grafica_dos.addWidget(self.grafica1)
+        self.ui.grafica_tres.addWidget(self.grafica2)
+        self.ui.grafica_cuatro.addWidget(self.grafica3)
+
+class Canvas_grafica(FigureCanvas):
+    def __init__(self, parent=None):     
+        self.fig , self.ax = plt.subplots(1, dpi=100, figsize=(5, 5), 
+            sharey=True, facecolor='white')
+        super().__init__(self.fig) 
+
+        nombres = ['15', '25', '30', '35','40']
+        colores = ['red','red','red','red', 'red']
+        tamaño = [10, 15, 20, 25, 30]
+
+        self.ax.bar(nombres, tamaño, color = colores)
+        self.fig.suptitle('Grafica de Barras',size=9)
+
+
+class Canvas_grafica2(FigureCanvas):
+    def __init__(self, parent=None):     
+        self.fig , self.ax = plt.subplots(1,dpi=100, figsize=(5, 5), 
+            sharey=True, facecolor='white')
+        super().__init__(self.fig) 
+
+        nombres = ['Fresa', 'Piña', 'Lima', 'Uva']
+        colores = ['blue','yellow','aqua','fuchsia']
+        tamaño = [20, 26, 30, 24]
+        explotar = [0.05, 0.05, 0.05, 0.05] 
+
+        plt.title("Cantidad de Frutas Disponibles",
+            color='black',size=9, family="Arial")
+
+        self.ax.pie(tamaño, explode = explotar, labels = nombres, 
+            colors = colores,
+                autopct = '%1.0f%%', pctdistance = 0.6,
+                shadow=True, startangle=90, radius = 0.8, 
+                labeldistance=1.1)  
+        self.ax.axis('equal')
+
+
+class Canvas_grafica3(FigureCanvas):
+    def __init__(self, parent=None):     
+        self.fig , self.ax = plt.subplots(1, dpi=100, figsize=(5, 5), 
+            sharey=True, facecolor='white')
+        super().__init__(self.fig) 
+
+        self.fig.suptitle('Grafica de Datos',size=9)
+        np.random.seed(20)
+        y = np.random.randn(150).cumsum()
+
+        self.ax = plt.axes()
+        plt.plot(y, color='magenta')
+
+
+class Canvas_grafica4(FigureCanvas):
+    def __init__(self, parent=None):     
+        self.fig , self.ax = plt.subplots(1, dpi=100, figsize=(5, 5), 
+            sharey=True, facecolor='white')
+        super().__init__(self.fig) 
+
+        x = [1, 2, 3, 4, 5,6,7]
+        y1 = [1, 0, 1, 3, 2,4,3]
+        y2 = [0, 2, 2, 3, 4,6,5]
+        y3 = [3, 1, 3, 4, 2,7,6]
+
+        y = np.vstack([y1, y2, y3])
+
+        labels = ["Y1 ", "Y2", "Y3"]
+        color = ["orange","blue","green"]
+
+        self.ax.stackplot(x, y1, y2, y3, labels=labels,colors=color)
+        self.ax.legend(loc='upper left')
+        self.ax.stackplot(x, y)
+        self.fig.suptitle('Grafica Stackplot',size=9)
+
+if __name__ == "__main__":
+     app = QApplication(sys.argv)
+     mi_app = MiApp()
+     mi_app.show()
+     sys.exit(app.exec_())  
+
---
+++ Graficas Matplotlib/ui_diseño.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'dise�odnqowz.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(622, 497)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setStyleSheet(u"background-color: rgb(0, 0, 127);")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.gridLayout = QGridLayout(self.frame)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setStyleSheet(u"QFrame{\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius:20px;\n"
+"border: 2px solid #000000;\n"
+"}")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_3 = QVBoxLayout(self.frame_2)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.label = QLabel(self.frame_2)
+        self.label.setObjectName(u"label")
+        self.label.setStyleSheet(u"QLabel{\n"
+"\n"
+"border: none;\n"
+"}")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_3.addWidget(self.label)
+
+        self.grafica_uno = QVBoxLayout()
+        self.grafica_uno.setObjectName(u"grafica_uno")
+
+        self.verticalLayout_3.addLayout(self.grafica_uno)
+
+        self.verticalLayout_3.setStretch(0, 1)
+        self.verticalLayout_3.setStretch(1, 5)
+
+        self.gridLayout.addWidget(self.frame_2, 0, 0, 1, 1)
+
+        self.frame_3 = QFrame(self.frame)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setStyleSheet(u"QFrame{\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius:20px;\n"
+"border: 2px solid #000000;\n"
+"}")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_4 = QVBoxLayout(self.frame_3)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.label_2 = QLabel(self.frame_3)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setStyleSheet(u"QLabel{\n"
+"\n"
+"border: none;\n"
+"}")
+        self.label_2.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_4.addWidget(self.label_2)
+
+        self.grafica_dos = QVBoxLayout()
+        self.grafica_dos.setObjectName(u"grafica_dos")
+
+        self.verticalLayout_4.addLayout(self.grafica_dos)
+
+        self.verticalLayout_4.setStretch(0, 1)
+        self.verticalLayout_4.setStretch(1, 5)
+
+        self.gridLayout.addWidget(self.frame_3, 0, 1, 1, 1)
+
+        self.frame_4 = QFrame(self.frame)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setStyleSheet(u"QFrame{\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius:20px;\n"
+"border: 2px solid #000000;\n"
+"}")
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_6 = QVBoxLayout(self.frame_4)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.label_3 = QLabel(self.frame_4)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setStyleSheet(u"QLabel{\n"
+"\n"
+"border: none;\n"
+"}")
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_6.addWidget(self.label_3)
+
+        self.grafica_tres = QVBoxLayout()
+        self.grafica_tres.setObjectName(u"grafica_tres")
+
+        self.verticalLayout_6.addLayout(self.grafica_tres)
+
+        self.verticalLayout_6.setStretch(0, 1)
+        self.verticalLayout_6.setStretch(1, 5)
+
+        self.gridLayout.addWidget(self.frame_4, 1, 0, 1, 1)
+
+        self.frame_5 = QFrame(self.frame)
+        self.frame_5.setObjectName(u"frame_5")
+        self.frame_5.setStyleSheet(u"QFrame{\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius:20px;\n"
+"border: 2px solid #000000;\n"
+"}")
+        self.frame_5.setFrameShape(QFrame.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_8 = QVBoxLayout(self.frame_5)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.label_4 = QLabel(self.frame_5)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setStyleSheet(u"QLabel{\n"
+"\n"
+"border: none;\n"
+"}")
+        self.label_4.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_8.addWidget(self.label_4)
+
+        self.grafica_cuatro = QVBoxLayout()
+        self.grafica_cuatro.setObjectName(u"grafica_cuatro")
+
+        self.verticalLayout_8.addLayout(self.grafica_cuatro)
+
+        self.verticalLayout_8.setStretch(0, 1)
+        self.verticalLayout_8.setStretch(1, 5)
+
+        self.gridLayout.addWidget(self.frame_5, 1, 1, 1, 1)
+
+
+        self.verticalLayout.addWidget(self.frame)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Graficas Matplotlib", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Grafica N\u00b0 1", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Grafica N\u00b0 2", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Grafica N\u00b0 3", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Grafica N\u00b0 4", None))
+    # retranslateUi
+
